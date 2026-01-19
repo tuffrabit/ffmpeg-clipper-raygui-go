f404a706fdce09e2c5ede31ff2ac8218fef239c0
Robbie Boucher <tuffrabit@gmail.com>
2026-01-19 17:08:34 -0500

    updated build flags

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ setlocal
 SET "CGO_ENABLED=1"
 SET "GOOS=windows"
 SET "GOARCH=amd64"
-SET "CC=zig cc -lc -target x86_64-windows-gnu -D__TIMESIZE=64 -D__LIBC -Wno-macro-redefined -Wno-dll-attribute-on-redeclaration -Wtautological-compare"
-SET "CXX=zig c++ -lc -target x86_64-windows-gnu -D__TIMESIZE=64 -D__LIBC -Wno-macro-redefined -Wno-dll-attribute-on-redeclaration -Wtautological-compare"
-go build
+SET "CC=zig cc -lc -target x86_64-windows-gnu -Wl,--subsystem,windows -D__TIMESIZE=64 -D__LIBC -Wno-macro-redefined -Wno-dll-attribute-on-redeclaration -Wtautological-compare"
+SET "CXX=zig c++ -lc -target x86_64-windows-gnu -Wl,--subsystem,windows -D__TIMESIZE=64 -D__LIBC -Wno-macro-redefined -Wno-dll-attribute-on-redeclaration -Wtautological-compare"
+go build -ldflags "-s -w -H windowsgui"
 endlocal
--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ GOOS=linux \
 GOARCH=amd64 \
 CC="zig cc -lc -target x86_64-linux-gnu.2.39 -D__TIMESIZE=64 -D__LIBC -Wno-macro-redefined -I/usr/include -L/usr/lib -L/usr/lib64 -L/lib/x86_64-linux-gnu" \
 CXX="zig c++ -lc -target x86_64-linux-gnu.2.39 -D__TIMESIZE=64 -D__LIBC -Wno-macro-redefined -I/usr/include -L/usr/lib -L/usr/lib64 -L/lib/x86_64-linux-gnu" \
-go build
+go build -ldflags "-s -w"
